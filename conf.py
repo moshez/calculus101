886219aba88d30897113b990268ab949e45559f8
Moshe Zadka <zadka.moshe@gmail.com>
2015-03-30 22:12:46 -0700

    chapter one works

--- conf.py
+++ conf.py
@@ -1,9 +1,8 @@
 # Copyright (c) Moshe Zadka
 # See LICENSE for details.
 
+extensions = ['sphinx.ext.mathjax']
 master_doc = 'index'
 project = 'Calculus 101'
 copyright = 'Copyright (c) 2015, Moshe Zadka'
 author = 'Moshe Zadka'
-##version = module.__version__
-##release = module.__version__
