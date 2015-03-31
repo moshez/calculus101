dc1977984e4374136803917f7ecf9a1e901e7042
Moshe Zadka <zadka.moshe@gmail.com>
2015-03-30 21:49:45 -0700

    temp commit

---
+++ conf.py
@@ -0,0 +1,9 @@
+# Copyright (c) Moshe Zadka
+# See LICENSE for details.
+
+master_doc = 'index'
+project = 'Calculus 101'
+copyright = 'Copyright (c) 2015, Moshe Zadka'
+author = 'Moshe Zadka'
+##version = module.__version__
+##release = module.__version__
---
+++ index.rst
@@ -0,0 +1,10 @@
+.. Copyright (c) Moshe Zadka
+   See LICENSE for details.
+
+Calculus 101
+============
+
+.. toctree::
+   :maxdepth: 2
+
+.. include:: history.rst
